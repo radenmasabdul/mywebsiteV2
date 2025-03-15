6ad322ee378fe8abbcbecc452a763448f3016223
radenmasabdul <abdul.tamsis@gmail.com>
2025-03-15 20:01:02 +0700

    remove radix ui

--- src/layouts/Layout.tsx
+++
@@ -1,5 +0,0 @@
-import { Theme } from "@radix-ui/themes";
-
-export function Layout({ children }: { children: React.ReactNode }) {
-  return <Theme>{children}</Theme>;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.tsx";
-import "@radix-ui/themes/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
