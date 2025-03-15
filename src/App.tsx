15d818f6d62859f228a011f819779933f241637e
radenmasabdul <abdul.tamsis@gmail.com>
2025-03-15 21:21:33 +0700

    feat: add component menu navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import Layout from "./layouts/Layout";
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold text-red-500 font-plus-jakarta">
-        Halo, Abdul! 🚀
-      </h1>
-    </div>
+    <>
+      <Layout />
+    </>
   );
 }
 
---
+++ src/components/Menu.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import {
+  Moon,
+  Sun,
+  Home,
+  Briefcase,
+  BookOpen,
+  MessageSquare,
+} from "lucide-react";
+
+interface MenuProps {
+  className?: string;
+}
+
+export default function Menu({ className = "" }: MenuProps) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <ul
+      className={`fixed top-4 z-50 menu menu-horizontal bg-[#1D1D1F] text-white rounded-4xl ${className}`}
+    >
+      <li>
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? (
+            <Sun className="h-5 w-5" />
+          ) : (
+            <Moon className="h-5 w-5" />
+          )}
+        </button>
+      </li>
+      <li>
+        <a href="#">
+          <Home className="h-5 w-5" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <Briefcase className="h-5 w-5" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <BookOpen className="h-5 w-5" />
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <MessageSquare className="h-5 w-5" />
+        </a>
+      </li>
+    </ul>
+  );
+}
